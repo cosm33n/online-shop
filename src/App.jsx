fe70b7c0a3cae577ec20d4ca44f991f65787f8ab
cosmin <cosm33n@gmail.com>
2023-01-20 14:16:38 +0200

    lesson1

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/* <img  alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='category-container'>
+        {/* <img  alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='category-container'>
+        {/* <img  alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img  alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Women</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img  alt="" /> */}
+        <div className='category-body-container'>
+          <h2>Men</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   )
 }
